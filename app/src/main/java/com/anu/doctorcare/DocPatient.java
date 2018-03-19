5f41b02a0db40edb8d9c0221350a8e7e4873b104
Anu <anuchoudhary6644@gmail.com>
2018-03-19 19:47:42 +0530

    Initial commit

---
+++ app/src/main/java/com/anu/doctorcare/DocPatient.java
@@ -0,0 +1,13 @@
+package com.anu.doctorcare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DocPatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doc_patient);
+    }
+}
